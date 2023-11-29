da69d3d3bde4985d8ab01c43404ce2556f9e363b
GamesTwoLifeTwoLife <vova08059910@gmail.com>
2023-11-29 13:18:33 +0200

    refactor: змінено typings

--- typings.ts
+++ typings.ts
@@ -1,5 +1,4 @@
 import * as Discord from "discord.js";
-import KazagumoClient from "./handlers/managers/KazagumoClient";
 declare type GuildDB = typeof import("./db/guilds");
 declare type UserDB = typeof import("./db/users");
 
@@ -8,43 +7,28 @@ declare type UserDB = typeof import("./db/users");
  */
 export interface MainClient extends Discord.Client {
     commands: Discord.Collection<string, Command>;
-    contextMenuCommands: Discord.Collection<string, ContextMenuCommand>;
     cooldowns: Discord.Collection<string, Discord.Collection<string, number>>;
     buttons: Discord.Collection<string, ButtonInteraction>;
     selectMenus: Discord.Collection<string, SelectMenuInteraction>;
     modals: Discord.Collection<string, ModalInteraction>;
     autocompletes: Discord.Collection<string, AutocompleteInteraction>;
-    manager: KazagumoClient;
     dbguild: GuildDB;
     dbuser: UserDB;
 };
 
 /**
- * Представляє (/) команду програми.
+ * Представляє команду програми.
  */
 export interface Command {
-    data: Discord.SlashCommandBuilder;
+    data: Discord.SlashCommandBuilder | Discord.ContextMenuCommandBuilder;
     options: {
         cooldown?: number,
         ownerOnly?: boolean,
+        devGuildOnly?: boolean,
         bot_permissions?: Discord.PermissionResolvable
     };
 
-    execute(interaction: Discord.ChatInputCommandInteraction & { client: MainClient }): void | Promise<void>;
-};
-
-/**
- * Представляє команду контекстного меню програми.
- */
-export interface ContextMenuCommand {
-    data: Discord.ContextMenuCommandBuilder;
-    options: {
-        cooldown?: number,
-        ownerOnly?: boolean,
-        bot_permissions?: Discord.PermissionResolvable
-    };
-
-    execute(interaction: Discord.ContextMenuCommandInteraction & { client: MainClient }): void | Promise<void>;
+    execute(interaction: (Discord.ChatInputCommandInteraction | Discord.ContextMenuCommandInteraction) & { client: MainClient }): void | Promise<void>;
 };
 
 /**
@@ -61,7 +45,7 @@ export interface ButtonInteraction {
 };
 
 /**
- * Представляє селект меню програми.
+ * Представляє меню вибору програми.
  */
 export interface SelectMenuInteraction {
     id: string;
