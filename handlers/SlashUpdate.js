1c8aaab5c9243191bf40091a2c3128de61df9820
GamesTwoLifeTwoLife <vova08059910@gmail.com>
2023-11-29 14:11:58 +0200

    fix: змінено slashUpdate

--- handlers/SlashUpdate.js
+++ handlers/SlashUpdate.js
@@ -13,14 +13,32 @@ module.exports = (client) => {
 
     (async () => {
         try {
-            console.log(`Розпочато оновлення ${commandsJSONData.length} глобальних та ${commandsOnlyTestGuildJSONData.length} серверних команд програми.`);
+            if (commandsJSONData.length === 0 && commandsOnlyTestGuildJSONData.length > 0) {
+                console.log(`Розпочато оновлення ${commandsOnlyTestGuildJSONData.length} серверних команд програми.`);
 
-            const [data_test_guild, data] = await Promise.all([
-                rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commandsOnlyTestGuildJSONData }),
-                rest.put(Routes.applicationCommands(clientId), { body: commandsJSONData })
-            ]);
+                const [data_test_guild] = await Promise.all([
+                    rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commandsOnlyTestGuildJSONData })
+                ]);
 
-            console.log(`Успішно перезавантажено ${data.length} глобальних та ${data_test_guild.length} серверних команд програми.`);
+                console.log(`Успішно перезавантажено ${data_test_guild.length} серверних команд програми.`);
+            } else if (commandsOnlyTestGuildJSONData.length === 0 && commandsJSONData.length > 0) {
+                console.log(`Розпочато оновлення ${commandsJSONData.length} глобальних серверних команд програми.`);
+
+                const [data] = await Promise.all([
+                    rest.put(Routes.applicationCommands(clientId), { body: commandsJSONData })
+                ]);
+
+                console.log(`Успішно перезавантажено ${data.length} глобальних команд програми.`);
+            } else {
+                console.log(`Розпочато оновлення ${commandsJSONData.length} глобальних та ${commandsOnlyTestGuildJSONData.length} серверних команд програми.`);
+
+                const [data_test_guild, data] = await Promise.all([
+                    rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commandsOnlyTestGuildJSONData }),
+                    rest.put(Routes.applicationCommands(clientId), { body: commandsJSONData })
+                ]);
+
+                console.log(`Успішно перезавантажено ${data.length} глобальних та ${data_test_guild.length} серверних команд програми.`);
+            }
         } catch (error) {
             console.error(error);
         }
